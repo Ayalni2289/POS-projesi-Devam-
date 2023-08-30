e93cd31223ae2f181fd18f8a4cc1a455d5933a18
Ayalniz2289 <152120191028@ogrenci.ogu.edu.tr>
2023-08-30 16:42:36 +0300

    products kısımı oluşturuluyor ayrıca grid sistemi uygulandı

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,24 @@
 import Categories from "./components/categories/categories";
 import Header from "./components/header/Header";
 import React from "react";
+import Products from "./components/products/Products";
 
 function App() {
   return (
     <>
+  {/*Header*/}
       <Header />
       <div className="home flex justify-between px-6 gap-10">
+  {/*Categories*/}      
         <div className="categories flex-1
                         overflow-auto max-h-[calc(100vh-111px)] pb-6">
           <Categories />
         </div>
+  {/*Products*/}
         <div className="products flex-[8]">
-          <div>products</div>
+          <Products />
         </div>
+  {/*Cart*/}
         <div className="cart">
           <div>cart</div>
         </div>
