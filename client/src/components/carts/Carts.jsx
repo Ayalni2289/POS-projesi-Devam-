add473e4314f8a1a9c9a40b740dcc9c03ccb1d0d
Ayalniz2289 <152120191028@ogrenci.ogu.edu.tr>
2023-09-01 15:55:17 +0300

    cart-item kısmı (sepetteki ürünler) oluşturuldu.

--- client/src/components/carts/Carts.jsx
+++ client/src/components/carts/Carts.jsx
@@ -1,14 +1,117 @@
 import { Button } from "antd"
-import { ClearOutlined } from "@ant-design/icons"
+import { ClearOutlined,
+PlusCircleOutlined 
+} from "@ant-design/icons"
 
 const Carts = () => {
     return (
         <div className="cart h-full max-h-[calc(100vh_-_90px)] flex flex-col">
             <h2 className="bg-blue-600 text-center py-4
                        text-white font-bold tracking-wide">Sepetim</h2>
-            <div className="cart-items">
-                <div className="cart-item">cart items</div>
-            </div>
+{/*Sepet İçeriği*/}
+            <ul className="cart-items px-2 flex flex-col gap-y-3 pt-2
+                           overflow-y-auto ">
+                <li className="cart-item flex justify-between">
+                <div className="flex items-center">
+                    <img src="https://img.piri.net/resim/upload/2020/09/26/12/21/24191133elma.jpg"
+                     alt=""  className="w-16 h-16 object-cover"/>
+                     <div className="flex flex-col ml-2">
+                        <b>Elma</b>
+                        <span>12₺ x 2</span>
+                     </div>
+                </div>
+                <div className="flex item-center gap-x-1">
+                <Button
+                    type="primary"
+                    size="small"
+                    className="w-full flex items-center justify-center !rounded-full"
+                    icon={<PlusCircleOutlined />}> 
+                </Button>
+                <span className="font-bold">1</span>
+                <Button
+                    type="primary"
+                    size="small"
+                    className="w-full flex items-center justify-center !rounded-full"
+                    icon={<PlusCircleOutlined />}>
+                </Button>
+                </div>
+                </li>
+                <li className="cart-item flex justify-between">
+                <div className="flex items-center">
+                    <img src="https://img.piri.net/resim/upload/2020/09/26/12/21/24191133elma.jpg"
+                     alt=""  className="w-16 h-16 object-cover"/>
+                     <div className="flex flex-col ml-2">
+                        <b>Elma</b>
+                        <span>12₺ x 2</span>
+                     </div>
+                </div>
+                <div className="flex item-center gap-x-1">
+                <Button
+                    type="primary"
+                    size="small"
+                    className="w-full flex items-center justify-center !rounded-full"
+                    icon={<PlusCircleOutlined />}> 
+                </Button>
+                <span className="font-bold">1</span>
+                <Button
+                    type="primary"
+                    size="small"
+                    className="w-full flex items-center justify-center !rounded-full"
+                    icon={<PlusCircleOutlined />}>
+                </Button>
+                </div>
+                </li>
+                <li className="cart-item flex justify-between">
+                <div className="flex items-center">
+                    <img src="https://img.piri.net/resim/upload/2020/09/26/12/21/24191133elma.jpg"
+                     alt=""  className="w-16 h-16 object-cover"/>
+                     <div className="flex flex-col ml-2">
+                        <b>Elma</b>
+                        <span>12₺ x 2</span>
+                     </div>
+                </div>
+                <div className="flex item-center gap-x-1">
+                <Button
+                    type="primary"
+                    size="small"
+                    className="w-full flex items-center justify-center !rounded-full"
+                    icon={<PlusCircleOutlined />}> 
+                </Button>
+                <span className="font-bold">1</span>
+                <Button
+                    type="primary"
+                    size="small"
+                    className="w-full flex items-center justify-center !rounded-full"
+                    icon={<PlusCircleOutlined />}>
+                </Button>
+                </div>
+                </li>
+                <li className="cart-item flex justify-between">
+                <div className="flex items-center">
+                    <img src="https://img.piri.net/resim/upload/2020/09/26/12/21/24191133elma.jpg"
+                     alt=""  className="w-16 h-16 object-cover"/>
+                     <div className="flex flex-col ml-2">
+                        <b>Elma</b>
+                        <span>12₺ x 2</span>
+                     </div>
+                </div>
+                <div className="flex item-center gap-x-1">
+                <Button
+                    type="primary"
+                    size="small"
+                    className="w-full flex items-center justify-center !rounded-full"
+                    icon={<PlusCircleOutlined />}> 
+                </Button>
+                <span className="font-bold">1</span>
+                <Button
+                    type="primary"
+                    size="small"
+                    className="w-full flex items-center justify-center !rounded-full"
+                    icon={<PlusCircleOutlined />}>
+                </Button>
+                </div>
+                </li>
+            </ul>
             <div className="cart-totals mt-auto"></div>
             <div className="border-t border-b mt-4">
 {/*Ara Toplam */}
