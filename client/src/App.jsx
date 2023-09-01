495dc54c50a0fa695a63de09e69b4bdd2e204a84
Ayalniz2289 <152120191028@ogrenci.ogu.edu.tr>
2023-09-02 00:10:54 +0300

    pages kısmında ayrı ayrı sayfaların etkietlerini ekledik

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,18 @@
-import Categories from "./components/categories/categories";
-import Header from "./components/header/Header";
-import React from "react";
-import Products from "./components/products/Products";
-import Carts from "./components/carts/Carts";
+import { BrowserRouter,Routes,Route  } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CartPage from "./pages/CartPage";
 
 function App() {
   return (
     <>
-  {/*Header*/}
-      <Header />
-      <div className="home flex justify-between flex-col md:flex-row px-6 gap-10 md:pb-0 pb-16">
-  {/*Categories*/}      
-        <div className="categories
-                        overflow-auto max-h-[calc(100vh-111px)] pb-6">
-          <Categories />
-        </div>
-  {/*Products*/}
-        <div className="products flex-[8] max-h-[calc(100vh-111px)] overflow-auto pb-6">
-          <Products />
-        </div>
-  {/*Cart*/}
-        <div className="cart-wrapper min-w-[300px] md:-mr-[24px] md:-mt-[24px] border">
-          < Carts />
-        </div>
-      </div>
+    <BrowserRouter>
+
+      <Routes>
+        <Route path="/home" element={<HomePage />}/>
+        <Route path="/cart" element={<CartPage />}/>
+      </Routes>
+
+    </BrowserRouter>
     </>
     );
 }
