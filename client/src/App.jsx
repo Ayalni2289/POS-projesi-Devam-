04bef06c98555c86b1c51f9b823ed31363c71903
Ayalniz2289 <152120191028@ogrenci.ogu.edu.tr>
2023-09-02 00:45:24 +0300

    carPage oluşturuldu ve düzenlendi tablo ve cart eklendi

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,18 @@
-import { BrowserRouter,Routes,Route  } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CartPage from "./pages/CartPage";
 
 function App() {
   return (
     <>
-    <BrowserRouter>
-
-      <Routes>
-        <Route path="/home" element={<HomePage />}/>
-        <Route path="/cart" element={<CartPage />}/>
-      </Routes>
-
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/cart" element={<CartPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
-    );
+  );
 }
 
 export default App;
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -6,25 +6,27 @@ import Carts from "../components/carts/Carts";
 const HomePage = () => {
   return (
     <>
-    {/*Header*/}
-    <Header />
-    <div className="home flex justify-between flex-col md:flex-row px-6 gap-10 md:pb-0 pb-16">
-{/*Categories*/}      
-      <div className="categories
-                      overflow-auto max-h-[calc(100vh-111px)] pb-6">
-        <Categories />
+      {/*Header*/}
+      <Header />
+      <div className="home flex justify-between flex-col md:flex-row px-6 gap-10 md:pb-0 pb-16">
+        {/*Categories*/}
+        <div
+          className="categories
+                      overflow-auto max-h-[calc(100vh-111px)] pb-6"
+        >
+          <Categories />
+        </div>
+        {/*Products*/}
+        <div className="products flex-[8] max-h-[calc(100vh-111px)] overflow-auto pb-6">
+          <Products />
+        </div>
+        {/*Cart*/}
+        <div className="cart-wrapper min-w-[300px] md:-mr-[24px] md:-mt-[24px] border">
+          <Carts />
+        </div>
       </div>
-{/*Products*/}
-      <div className="products flex-[8] max-h-[calc(100vh-111px)] overflow-auto pb-6">
-        <Products />
-      </div>
-{/*Cart*/}
-      <div className="cart-wrapper min-w-[300px] md:-mr-[24px] md:-mt-[24px] border">
-        < Carts />
-      </div>
-    </div>
     </>
   );
-}
+};
 
 export default HomePage;
