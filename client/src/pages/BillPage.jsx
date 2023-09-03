0362c733081013e3f1a6623d54d1bf5052d0f2fa
Ayalniz2289 <152120191028@ogrenci.ogu.edu.tr>
2023-09-03 16:17:06 +0300

    Fatura yazdırma modal'ı eklendi

--- client/src/pages/BillPage.jsx
+++ client/src/pages/BillPage.jsx
@@ -1,4 +1,4 @@
-import { Table, Card, Button} from "antd";
+import { Table, Card, Button } from "antd";
 import React, { useState } from "react";
 import Header from "../components/header/Header";
 import PrintBill from "../components/bills/PrintBill";
@@ -41,10 +41,22 @@ const BillPage = () => {
       <Header />
       <div>
         <h1 className="text-4xl font-bold text-center mb-4">Faturalar</h1>
-        <Table dataSource={dataSource} columns={columns} bordered pagination={false} />
+        <Table
+          dataSource={dataSource}
+          columns={columns}
+          bordered
+          pagination={false}
+        />
         <div className="cart-total flex justify-end mt-4">
           <Card className="w-72">
-            <Button className="w-full mt-3" size="large" type="primary" onClick={()=> setIsModalOpen(true)}>Yazdır</Button>
+            <Button
+              className="w-full mt-3"
+              size="large"
+              type="primary"
+              onClick={() => setIsModalOpen(true)}
+            >
+              Yazdır
+            </Button>
           </Card>
         </div>
       </div>
