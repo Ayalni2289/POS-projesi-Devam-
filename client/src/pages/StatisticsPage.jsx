17b4faf199ce5f05d38d30837494cf6bf8713a20
Ayalniz2289 <152120191028@ogrenci.ogu.edu.tr>
2023-09-07 13:36:04 +0300

    Responsive hale geldi chart kısımları

--- client/src/pages/StatisticsPage.jsx
+++ client/src/pages/StatisticsPage.jsx
@@ -1,30 +1,27 @@
 import { useEffect, useState } from "react";
-import Header from "../components/header/Header";
-import StatisticCard from "../components/statistics/StatisticCard";
-import { Area,Pie } from '@ant-design/plots';
+import Header from "../components/header/Header.jsx";
+import StatisticCard from "../components/statistics/StatisticCard.jsx";
+import { Area, Pie } from "@ant-design/plots";
 
-const StatisticsPage = () => {
+const StatisticPage = () => {
   const [data, setData] = useState([]);
+
   useEffect(() => {
     asyncFetch();
   }, []);
+
   const asyncFetch = () => {
-    fetch('https://gw.alipayobjects.com/os/bmw-prod/360c3eae-0c73-46f0-a982-4746a6095010.json')
+    fetch(
+      "https://gw.alipayobjects.com/os/bmw-prod/360c3eae-0c73-46f0-a982-4746a6095010.json"
+    )
       .then((response) => response.json())
       .then((json) => setData(json))
       .catch((error) => {
-        console.log('fetch data failed', error);
+        console.log("fetch data failed", error);
       });
   };
-  const config = {
-    data,
-    xField: 'timePeriod',
-    yField: 'value',
-    xAxis: {
-      range: [0, 1],
-    },
-  };
-  const data2  = [
+
+  const data2 = [
     {
       type: '分类一',
       value: 27,
@@ -50,52 +47,62 @@ const StatisticsPage = () => {
       value: 5,
     },
   ];
+
+  const config = {
+    data,
+    xField: "timePeriod",
+    yField: "value",
+    xAxis: {
+      range: [0, 1],
+    },
+  };
+
   const config2 = {
     appendPadding: 10,
     data: data2,
-    angleField: 'value',
-    colorField: 'type',
+    angleField: "value",
+    colorField: "type",
     radius: 1,
     innerRadius: 0.6,
     label: {
-      type: 'inner',
-      offset: '-50%',
-      content: '{value}',
+      type: "inner",
+      offset: "-50%",
+      content: "{value}",
       style: {
-        textAlign: 'center',
+        textAlign: "center",
         fontSize: 14,
       },
     },
     interactions: [
       {
-        type: 'element-selected',
+        type: "element-selected",
       },
       {
-        type: 'element-active',
+        type: "element-active",
       },
     ],
     statistic: {
       title: false,
       content: {
         style: {
-          whiteSpace: 'pre-wrap',
-          overflow: 'hidden',
-          textOverflow: 'ellipsis',
+          whiteSpace: "pre-wrap",
+          overflow: "hidden",
+          textOverflow: "ellipsis",
         },
-        content: 'AntV\nG2Plot',
+        content: "AntV\nG2Plot",
       },
     },
   };
-  
+
   return (
     <>
       <Header />
-      <div className="px-6">
+      <div className="px-6 md:pb-0 pb-20">
         <h1 className="text-4xl font-bold text-center mb-4">İstatistiklerim</h1>
         <div className="statistic-section">
           <h2 className="text-lg">
-            Hoşgeldiniz{" "}
-            <span className="text-green-700 font-bold text-xl">admin</span>
+            Hoş geldin{" "}
+            <span className="text-green-700 font-bold text-xl">admin</span>.
           </h2>
           <div className="statistic-cards grid xl:grid-cols-4 md:grid-cols-2 my-10 md:gap-10 gap-4">
             <StatisticCard
@@ -105,28 +112,32 @@ const StatisticsPage = () => {
             />
             <StatisticCard
               title={"Toplam Kazanç"}
-              amount={"200.00 ₺"}
+              amount={"660.96 ₺"}
               img={"images/money.png"}
             />
             <StatisticCard
               title={"Toplam Satış"}
-              amount={"10"}
+              amount={"6"}
               img={"images/sale.png"}
             />
             <StatisticCard
               title={"Toplam Ürün"}
-              amount={"30"}
+              amount={"28"}
               img={"images/product.png"}
             />
-        </div>
-        <div className="">
-        <div><Area {...config} />;</div>
-        <div><Pie {...config2} />;</div>
-        </div>            
+          </div>
+          <div className="flex justify-between gap-10 lg:flex-row flex-col items-center">
+            <div className="lg:w-1/2 lg:h-500 h-72">
+              <Area {...config} />
+            </div>
+            <div className="lg:w-1/2 lg:h-500 h-72">
+              <Pie {...config2} />
+            </div>
+          </div>
         </div>
       </div>
     </>
   );
 };
 
-export default StatisticsPage;
+export default StatisticPage;
