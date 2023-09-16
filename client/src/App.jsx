70d91d427089c031ef76cfc7d01bac4ad47d98a8
Ayalniz2289 <152120191028@ogrenci.ogu.edu.tr>
2023-09-16 14:16:05 +0300

    Login sayfası oluşturuldu.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import BillPage from "./pages/BillPage";
 import CustomerPage from "./pages/CustomerPage";
 import StatisticsPage from "./pages/StatisticsPage";
 import Register from "./pages/auth/Register";
+import Login from "./pages/auth/Login";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path="/customers" element={<CustomerPage />} />
           <Route path="/statistics" element={<StatisticsPage />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </>
--- client/src/pages/auth/Login.jsx
+++ client/src/pages/auth/Login.jsx
@@ -1,26 +1,15 @@
-import { Button, Carousel, Form, Input } from "antd";
+import { Button, Carousel, Checkbox, Form, Input } from "antd";
 import { Link } from "react-router-dom";
 import AuthCarousel from "../../components/auth/AuthCarousel";
+import FormItem from "antd/es/form/FormItem";
 
-const Register = () => {
+const Login = () => {
   return (
     <div className="h-screen">
       <div className="flex justify-between h-full">
         <div className="xl:px-20 px-10 w-full flex flex-col h-full justify-center relative">
           <h1 className="text-center text-5xl font-bold mb-2">LOGO</h1>
           <Form layout="vertical">
-            <Form.Item
-              label="Kullanıcı Adı"
-              name={"username"}
-              rules={[
-                {
-                  required: true,
-                  message: "Lütfen kullanıcı adınızı giriniz!",
-                },
-              ]}
-            >
-              <Input />
-            </Form.Item>
             <Form.Item
               label="E-mail"
               name={"email"}
@@ -45,18 +34,12 @@ const Register = () => {
             >
               <Input.Password />
             </Form.Item>
-            <Form.Item
-              label="Şifre Tekrar"
-              name={"passwordAgain"}
-              rules={[
-                {
-                  required: true,
-                  message: "Lütfen Şifreyi tekrar giriniz!",
-                },
-              ]}
-            >
-              <Input.Password />
-            </Form.Item>
+            <FormItem>
+                <div className="flex justify-between items-center">
+                    <Checkbox>Beni Hatırla </Checkbox>
+                    <Link>Şifremi Unuttum</Link>
+                </div>
+            </FormItem>
             <Form.Item>
               <Button
                 type="primary"
@@ -64,16 +47,17 @@ const Register = () => {
                 className="w-full"
                 size="large"
               >
-                Kaydol
+                Giriş Yap
               </Button>
             </Form.Item>
           </Form>
           <div className="flex justify-center absolute left-0 bottom-10 w-full">
-            Bir hesabınız mı var ?&nbsp;{" "}
-            <Link className="text-blue-600" to="/login">
-              Şimdi giriş yapın.
+            Bir hesabınız yok mu ?&nbsp;{" "}
+            <Link className="text-blue-600" to="/register">
+              Şimdi Kaydolun.
             </Link>
           </div>
+          
         </div>
         <div className="xl:w-4/6 lg:w-3/5 md:w-1/2 md:flex hidden bg-[#6c63ff] h-full">
           <div className="w-full h-full flex items-center">
@@ -108,4 +92,4 @@ const Register = () => {
   );
 };
 
-export default Register;
+export default Login;
