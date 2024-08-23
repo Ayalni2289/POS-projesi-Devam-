33a0cd17a3f567fd682fc512ba3a847a2af467cf
Ayalni2289 <alininspor@hotmail.com>
2024-08-23 15:55:37 +0300

    logo güncellemesi yapıldı ve renk güncellemeleri yapıldı ayrıca coursel autoplay özelliiği eklendi

--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -34,7 +34,12 @@ const Register = () => {
     <div className="h-screen">
       <div className="flex justify-between h-full">
         <div className="xl:px-20 px-10 w-full flex flex-col h-full justify-center relative">
-          <h1 className="text-center text-5xl font-bold mb-2">LOGO</h1>
+          <img style={{
+            width: "150px",
+            height: "150px",
+            objectFit: "contain",
+            margin: "0 auto",
+          }} src="/images/foodLogo.png" alt="logo" />
           <Form layout="vertical" onFinish={onFinish}>
             <Form.Item
               label="Kullanıcı Adı"
@@ -112,15 +117,15 @@ const Register = () => {
           </Form>
           <div className="flex justify-center absolute left-0 bottom-10 w-full">
             Bir hesabınız mı var ?&nbsp;{" "}
-            <Link className="text-blue-600" to="/login">
+            <Link className="text-green-400 hover:text-green-700" to="/login">
               Şimdi giriş yapın.
             </Link>
           </div>
         </div>
-        <div className="xl:w-4/6 lg:w-3/5 md:w-1/2 md:flex hidden bg-[#6c63ff] h-full">
+        <div className="xl:w-4/6 lg:w-3/5 md:w-1/2 md:flex hidden bg-[#19bec1] h-full">
           <div className="w-full h-full flex items-center">
             <div className="w-full">
-              <Carousel className="!h-full px-6">
+              <Carousel autoplay className="!h-full px-6">
                 <AuthCarousel
                   img="/images/responsive.svg"
                   title="Responsive"
